6b38717ce6fbbfc521369696744ccc583b50f105
shakilgaribook <shakilhossaingaribook@gmail.com>
2026-08-16 14:09:44 +0600

    Update realtime schema migration

--- migrations/000003_realtime_schema.sql
+++ migrations/000003_realtime_schema.sql
@@ -18,6 +18,12 @@ CREATE TABLE messages (
     sended_by VARCHAR(20) CHECK (sended_by IN ('ADMIN', 'CUSTOMER', 'DRIVER')) NOT NULL,
     content TEXT NOT NULL,
     seen BOOLEAN DEFAULT FALSE,
+    voice_messages TEXT,
+    photo TEXT,
+    file TEXT,
+    full_name VARCHAR(255),
+    profile_picture TEXT,
+    gender VARCHAR(10),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
